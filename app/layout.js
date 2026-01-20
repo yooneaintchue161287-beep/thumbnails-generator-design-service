c29257209cd1fdccdf76ad90a82372e08be67c1f
yooneaintchue161287-beep <yooneaintchue161287@gmail.com>
2026-01-20 21:20:39 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "ThumbnailBoost – Get More Views",
+  description: "High CTR YouTube thumbnails"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
