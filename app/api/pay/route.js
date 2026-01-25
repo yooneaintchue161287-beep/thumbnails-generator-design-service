f2d29a1d5697a46a7748372bb0950b1885704f44
yooneaintchue161287-beep <yooneaintchue161287@gmail.com>
2026-01-25 21:21:34 +0800

    Create route.js

---
+++ app/api/pay/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  const res = await fetch("https://api.paymongo.com/v1/links", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Basic " +
+        Buffer.from(process.env.PAYMONGO_SECRET_KEY + ":").toString("base64"),
+    },
+    body: JSON.stringify({
+      data: {
+        attributes: {
+          amount: 500,
+          description: "Thumbnail Unlock",
+        },
+      },
+    }),
+  });
+
+  const data = await res.json();
+  return NextResponse.json(data);
+}
